1706e85b2c732f88145b95a12ee3a8e9f5afca46
Kanemura775 <milkchocolate0141775@gmail.com>
2021-07-02 16:20:08 +0900

    [3の倍数]を学習しました.

---
+++ 01_テキスト/01_JavaScriptのイベント/main.js
@@ -0,0 +1,21 @@
+const alertMessage = function() {
+    alert("クリックしたね")
+}
+
+
+const button = document.getElementById("butoon")
+
+const alertMessage = funcion(),{
+    alert("クリックしたね")
+}
+
+button.onclick = alertMessage
+
+const inputText = document.getElementById("input-text")
+const inputDate = document.getElementById("inout-date")
+
+const sayHello = function() {
+    console.log("hello")
+}
+inputText.oninput = sayHello
+inpurDate.oninput = sayHello
