175bfd60b1d391358e99e4315c29cb9ea7c0cde4
Kanemura775 <milkchocolate0141775@gmail.com>
2021-07-07 20:00:04 +0900

    BuzzFizz

--- 04_ゼミ/week1/week1 フォローアップ/object.js
+++ 04_ゼミ/week1/week1 フォローアップ/object.js
@@ -1,5 +1,18 @@
 const nanako = {
-    name:"ななこ"
-    age:"21"
-    family:""
+  name: "ななこ",
+  age: "21",
+  food: ["バナナ", "チョコレート", "わかめ"],
+  family: {
+    papa: "父",
+    mama: "母",
+    queen: "犬",
+  },
+  sayHello: function() {
+    alert("죽겠다")
+  },
 }
+nanako.sayHello()
+console.log(nanako.name)
+console.log(nanako.age)
+console.log(nanako.food)
+console.log(nanako.family.queen)
